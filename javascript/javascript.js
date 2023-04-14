e37257d6e62afa38a4b31253cc7a80dbf6f8ccea
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-14 14:51:22 +0200

    push

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,15 +4,17 @@ let num = parseInt(prompt('numero quadrati'))
 for (let i = 0; i <= num; i++) {
 	console.log(i);
 	eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+
+    if( i % 3 == 0) console.log('Fizz');
+
+    else if (i % 5 == 0) console.log ('Buzz');
+
+    else if ( i % 15 == 0) console.log ('FizzBuzz');
+
+    else (i)
+
+
 }
-for (var i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    document.querySelector('.cell').innerHTML = ('FizzBuzz')
-    if (i % 3 == 0) console.log("Fizz");
-    document.querySelector('.cell').innerHTML = ('Fizz')
-    
-     if (i % 5 == 0) console.log("Buzz");
-    document.querySelector('.cell').innerHTML = ('Buzz')
-     console.log(i);
-}
+
+
     
