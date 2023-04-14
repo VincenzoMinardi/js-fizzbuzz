b19a0b7c492e2d3a443c8129e19656b0224648c0
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-04-14 17:15:16 +0200

    add: completed layout grid with cells

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,19 +1,27 @@
 const eleGrid = document.querySelector('.grid');
-let num = parseInt(prompt('numero quadrati'))
 
-for (let i = 0; i <= num; i++) {
-	console.log(i);
-	eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+for (let i = 1; i <=100; i++) {
+    
+	
 
-    if( i % 3 == 0) console.log('Fizz');
-
-    else if (i % 5 == 0) console.log ('Buzz');
-
-    else if ( i % 15 == 0) console.log ('FizzBuzz');
-
-    else (i)
-
-
+    if ( i % 15 == 0){
+        console.log('FizzBuzz');
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell15">FizzBuzz</div>`;
+    
+    } 
+    else if( i % 3 == 0){
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell3">Fizz</div>`;
+        console.log('Fizz');
+    } 
+    else if (i % 5 == 0){
+        console.log('Buzz');
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell cell5">Buzz</div>`;
+    } 
+    else {
+        console.log (i)
+        eleGrid.innerHTML = eleGrid.innerHTML + `<div class="cell">${i}</div>`;
+    }
+    
 }
 
 
